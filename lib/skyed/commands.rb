f0579d86966fcb81a87c1eac938f0b6183d54457
Ignasi Fosch <natx@y10k.ws>
2015-03-24 18:17:24 +0100

    INF-706: Adds option to init to force remote

--- lib/skyed/commands.rb
+++ lib/skyed/commands.rb
@@ -2,8 +2,11 @@ desc 'Initialize skyed'
 long_desc 'Sets up skyed configuration for a repository'
 
 command :init do |cmd|
-  cmd.action do |global_options|
-    Skyed::Init.execute(global_options)
+  cmd.flag :remote, default_value: nil,
+                    type: String,
+                    desc: 'Remote to use in OpsWorks'
+  cmd.action do |global_options, options|
+    Skyed::Init.execute(global_options, options)
   end
 end
 
@@ -11,10 +14,7 @@ desc 'Deploy current setup'
 long_desc 'Deploys from current repository'
 
 command :deploy do |cmd|
-  cmd.flag :remote, default_value: nil,
-                    type: String,
-                    desc: 'Remote to use in OpsWorks'
-  cmd.action do |global_options, options|
-    Skyed::Deploy.execute(global_options, options)
+  cmd.action do |global_options|
+    Skyed::Deploy.execute(global_options)
   end
 end
