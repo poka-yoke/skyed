f855304d4deb1b106246185590e4bef353e67c0e
Ignasi Fosch <natx@y10k.ws>
2015-04-01 15:52:20 +0200

    INF-707: Modifies destroy to also deregister the instance

--- lib/skyed/destroy.rb
+++ lib/skyed/destroy.rb
@@ -3,7 +3,16 @@ module Skyed
   module Destroy
     class << self
       def execute(_global_options, _options, _args)
-        `cd #{Skyed::Settings.repo} && vagrant destroy -f`
+        repo_path = Skyed::Settings.repo
+        hostname = `cd #{repo_path} && vagrant ssh -c hostname`.strip
+        `cd #{repo_path} && vagrant destroy -f`
+        ow = Skyed::Init.ow_client
+        instances = ow.describe_instances(stack_id: Skyed::Settings.stack_id)
+        instances[:instances].each do |instance|
+          if instance[:hostname] == hostname
+            ow.deregister_instance(instance_id: instance[:instance_id])
+          end
+        end
       end
     end
   end
--- spec/destroy_spec.rb
+++ spec/destroy_spec.rb
@@ -3,20 +3,44 @@ require 'skyed'
 require 'highline/import'
 
 describe 'Skyed::Destroy.execute' do
-  let(:repo_path) { '/home/ifosch/opsworks' }
+  let(:opsworks)    { double('AWS::OpsWorks::Client') }
+  let(:repo_path)   { '/home/ifosch/opsworks' }
+  let(:hostname)    { 'test-ifosch' }
+  let(:stack_id)    { 'e1403a56-286e-4b5e-6798-c3406c947b4a' }
+  let(:instance_id) { '12345678-1234-4321-5678-210987654321' }
+  let(:instances) do
+    { instances: [{
+      instance_id: instance_id,
+      hostname: hostname,
+      stack_id: stack_id
+    }] }
+  end
   before(:each) do
     expect(Skyed::Settings)
       .to receive(:repo)
       .and_return(repo_path)
+    expect(Skyed::Destroy)
+      .to receive(:`)
+      .with("cd #{repo_path} && vagrant ssh -c hostname")
+      .and_return(hostname)
+    expect(Skyed::Destroy)
+      .to receive(:`)
+      .with("cd #{repo_path} && vagrant destroy -f")
+    expect(Skyed::Init)
+      .to receive(:ow_client)
+      .and_return(opsworks)
+    expect(Skyed::Settings)
+      .to receive(:stack_id)
+      .and_return(stack_id)
+    expect(opsworks)
+      .to receive(:describe_instances)
+      .with(stack_id: stack_id)
+      .and_return(instances)
+    expect(opsworks)
+      .to receive(:deregister_instance)
+      .with(instance_id: instance_id)
   end
-  context 'when vagrant machine is running' do
-    before(:each) do
-      expect(Skyed::Destroy)
-        .to receive(:`)
-        .with("cd #{repo_path} && vagrant destroy -f")
-    end
-    it 'destroys the vagrant machine' do
-      Skyed::Destroy.execute(nil, nil, nil)
-    end
+  it 'destroys the vagrant machine' do
+    Skyed::Destroy.execute(nil, nil, nil)
   end
 end
