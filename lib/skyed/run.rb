8a7dd10d191e845743fa3528b102e7ddab0749ce
Ignasi Fosch <natx@y10k.ws>
2015-04-07 15:14:42 +0200

    INF-594: Simplifies tests and code in Run module

--- lib/skyed/run.rb
+++ lib/skyed/run.rb
@@ -3,19 +3,31 @@ module Skyed
   module Run
     class << self
       def execute(global_options, options, args)
-        run(global_options, options, args) unless Skyed::Settings.empty?
-        recipes = args.select { |recipe| recipe_in_cookbook(recipe) }
-        msg = "Couldn't found #{args - recipes} recipes in repository"
-        fail msg unless recipes == args
+        if Skyed::Settings.empty?
+          run(global_options, options, args)
+        else
+          recipes = check_recipes_exist(args)
+          check_vagrant
+          execute_recipes(recipes)
+        end
+      end
+
+      def run(_global_options, _options, _args)
+      end
+
+      def check_vagrant
         output = `cd #{Skyed::Settings.repo} && vagrant status`
         msg = 'Vagrant failed'
         fail msg unless $CHILD_STATUS.success?
         msg = 'Vagrant machine is not running'
         fail msg unless output =~ /running/
-        execute_recipes(recipes)
       end
 
-      def run(_global_options, _options, _args)
+      def check_recipes_exist(args)
+        recipes = args.select { |recipe| recipe_in_cookbook(recipe) }
+        msg = "Couldn't found #{args - recipes} recipes in repository"
+        fail msg unless recipes == args
+        recipes
       end
 
       def execute_recipes(recipes)
--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -5,127 +5,23 @@ require 'highline/import'
 describe 'Skyed::Run.execute' do
   context 'when initialized' do
     let(:recipe1) { 'recipe1' }
+    let(:args)      { [recipe1] }
     before(:each) do
       expect(Skyed::Settings)
         .to receive(:empty?)
         .and_return(false)
+      expect(Skyed::Run)
+        .to receive(:check_recipes_exist)
+        .with(args)
+        .and_return([recipe1])
+      expect(Skyed::Run)
+        .to receive(:check_vagrant)
+      expect(Skyed::Run)
+        .to receive(:execute_recipes)
+        .with(args)
     end
-    context 'and invoked with valid recipe' do
-      let(:args)      { [recipe1] }
-      let(:repo_path) { '/home/ifosch/opsworks' }
-      before(:each) do
-        expect(Skyed::Run)
-          .to receive(:recipe_in_cookbook)
-          .with(recipe1)
-          .and_return(true)
-        expect(Skyed::Settings)
-          .to receive(:repo)
-          .and_return(repo_path)
-      end
-      context 'and vagrant is running' do
-        let(:opsworks)      { double('Aws::OpsWorks::Client') }
-        let(:access)        { 'AKIAAKIAAKIA' }
-        let(:secret)        { 'sGe84ofDSkfo' }
-        let(:stack_id)      { 'df345d54-75b4-431b-adb2-eb6b9e549283' }
-        let(:cmd_args)      { { 'recipes' => [recipe1] } }
-        let(:cmd)           { { name: 'execute_recipes', args: cmd_args } }
-        let(:deployment_id) { 'de305d54-75b4-431b-adb2-eb6b9e546013' }
-        before(:each) do
-          expect(Skyed::Run)
-            .to receive(:`)
-            .with("cd #{repo_path} && vagrant status")
-            .and_return('Any output containing running')
-          expect(Skyed::Init)
-            .to receive(:ow_client)
-            .and_return(opsworks)
-          expect(Skyed::Settings)
-            .to receive(:stack_id)
-            .and_return(stack_id)
-          expect(opsworks)
-            .to receive(:create_deployment)
-            .with(stack_id: stack_id, command: cmd)
-            .and_return(deployment_id: deployment_id)
-        end
-        it 'runs the recipe' do
-          Skyed::Run.execute(nil, nil, args)
-        end
-      end
-      context 'and vagrant is not running' do
-        before(:each) do
-          expect(Skyed::Run)
-            .to receive(:`)
-            .with("cd #{repo_path} && vagrant status")
-            .and_return('Any output containing anything else')
-        end
-        it 'fails' do
-          expect { Skyed::Run.execute(nil, nil, args) }
-            .to raise_error
-        end
-      end
-      context 'but vagrant fails' do
-        before(:each) do
-          expect(Skyed::Run)
-            .to receive(:`)
-            .with("cd #{repo_path} && vagrant status")
-            .and_return('Any output failing')
-          expect($CHILD_STATUS)
-            .to receive(:success?)
-            .and_return(false)
-        end
-        it 'fails' do
-          expect { Skyed::Run.execute(nil, nil, args) }
-            .to raise_error
-        end
-      end
-    end
-    context 'and invoked with unexisting recipe' do
-      let(:args)   { [recipe1] }
-      before(:each) do
-        expect(Skyed::Run)
-          .to receive(:recipe_in_cookbook)
-          .with(recipe1)
-          .and_return(false)
-      end
-      it 'fails' do
-        expect { Skyed::Run.execute(nil, nil, args) }
-          .to raise_error
-      end
-    end
-    context 'and invoked with unexisting recipes' do
-      let(:recipe2) { 'recipe2::restart' }
-      let(:args)   { [recipe1, recipe2] }
-      before(:each) do
-        expect(Skyed::Run)
-          .to receive(:recipe_in_cookbook)
-          .with(recipe1)
-          .and_return(false)
-        expect(Skyed::Run)
-          .to receive(:recipe_in_cookbook)
-          .with(recipe2)
-          .and_return(false)
-      end
-      it 'fails' do
-        expect { Skyed::Run.execute(nil, nil, args) }
-          .to raise_error
-      end
-    end
-    context 'and invoked with existing and unexisting recipes' do
-      let(:recipe2) { 'recipe2::restart' }
-      let(:args)   { [recipe1, recipe2] }
-      before(:each) do
-        expect(Skyed::Run)
-          .to receive(:recipe_in_cookbook)
-          .with(recipe1)
-          .and_return(false)
-        expect(Skyed::Run)
-          .to receive(:recipe_in_cookbook)
-          .with(recipe2)
-          .and_return(true)
-      end
-      it 'fails' do
-        expect { Skyed::Run.execute(nil, nil, args) }
-          .to raise_error
-      end
+    it 'runs the recipe' do
+      Skyed::Run.execute(nil, nil, args)
     end
   end
   context 'when not initialized' do
@@ -133,6 +29,151 @@ describe 'Skyed::Run.execute' do
       expect(Skyed::Settings)
         .to receive(:empty?)
         .and_return(true)
+      expect(Skyed::Run)
+        .to receive(:run)
+    end
+    it 'uses run method' do
+      Skyed::Run.execute(nil, nil, nil)
+    end
+  end
+end
+
+describe 'Skyed::Run.check_vagrant' do
+  let(:repo_path) { '/home/ifosch/opsworks' }
+  before(:each) do
+    expect(Skyed::Settings)
+      .to receive(:repo)
+      .and_return(repo_path)
+  end
+  context 'when vagrant is running' do
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:`)
+        .with("cd #{repo_path} && vagrant status")
+        .and_return('Any output containing running')
+    end
+    it 'runs ok' do
+      Skyed::Run.check_vagrant
+    end
+  end
+  context 'when vagrant is not running' do
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:`)
+        .with("cd #{repo_path} && vagrant status")
+        .and_return('Any output containing anything else')
+    end
+    it 'fails' do
+      expect { Skyed::Run.check_vagrant }
+        .to raise_error
+    end
+  end
+  context 'when vagrant fails' do
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:`)
+        .with("cd #{repo_path} && vagrant status")
+        .and_return('Any output failing')
+      expect($CHILD_STATUS)
+        .to receive(:success?)
+        .and_return(false)
+    end
+    it 'fails' do
+      expect { Skyed::Run.check_vagrant }
+        .to raise_error
+    end
+  end
+end
+
+describe 'Skyed::Run.execute_recipes' do
+  let(:recipe1)       { 'recipe1' }
+  let(:opsworks)      { double('Aws::OpsWorks::Client') }
+  let(:access)        { 'AKIAAKIAAKIA' }
+  let(:secret)        { 'sGe84ofDSkfo' }
+  let(:stack_id)      { 'df345d54-75b4-431b-adb2-eb6b9e549283' }
+  let(:cmd_args)      { { 'recipes' => [recipe1] } }
+  let(:cmd)           { { name: 'execute_recipes', args: cmd_args } }
+  let(:deployment_id) { 'de305d54-75b4-431b-adb2-eb6b9e546013' }
+  before(:each) do
+    expect(Skyed::Init)
+      .to receive(:ow_client)
+      .and_return(opsworks)
+    expect(Skyed::Settings)
+      .to receive(:stack_id)
+      .and_return(stack_id)
+    expect(opsworks)
+      .to receive(:create_deployment)
+      .with(stack_id: stack_id, command: cmd)
+      .and_return(deployment_id: deployment_id)
+  end
+  it 'runs the recipe' do
+    Skyed::Run.execute_recipes([recipe1])
+  end
+end
+
+describe 'Skyed::Run.check_recipes_exist' do
+  let(:recipe1) { 'recipe1' }
+  context 'when invoked with valid recipe' do
+    let(:args)      { [recipe1] }
+    let(:repo_path) { '/home/ifosch/opsworks' }
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:recipe_in_cookbook)
+        .with(recipe1)
+        .and_return(true)
+    end
+    it 'runs the recipe' do
+      expect(Skyed::Run.check_recipes_exist(args))
+        .to eq [recipe1]
+    end
+  end
+  context 'when invoked with unexisting recipe' do
+    let(:args)   { [recipe1] }
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:recipe_in_cookbook)
+        .with(recipe1)
+        .and_return(false)
+    end
+    it 'fails' do
+      expect { Skyed::Run.check_recipes_exist(args) }
+        .to raise_error
+    end
+  end
+  context 'when invoked with unexisting recipes' do
+    let(:recipe2) { 'recipe2::restart' }
+    let(:args)   { [recipe1, recipe2] }
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:recipe_in_cookbook)
+        .with(recipe1)
+        .and_return(false)
+      expect(Skyed::Run)
+        .to receive(:recipe_in_cookbook)
+        .with(recipe2)
+        .and_return(false)
+    end
+    it 'fails' do
+      expect { Skyed::Run.check_recipes_exist(args) }
+        .to raise_error
+    end
+  end
+  context 'when invoked with existing and unexisting recipes' do
+    let(:recipe2) { 'recipe2::restart' }
+    let(:args)   { [recipe1, recipe2] }
+    before(:each) do
+      expect(Skyed::Run)
+        .to receive(:recipe_in_cookbook)
+        .with(recipe1)
+        .and_return(false)
+      expect(Skyed::Run)
+        .to receive(:recipe_in_cookbook)
+        .with(recipe2)
+        .and_return(true)
+    end
+    it 'fails' do
+      expect { Skyed::Run.check_recipes_exist(args) }
+        .to raise_error
     end
   end
 end
