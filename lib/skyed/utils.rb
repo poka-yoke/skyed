28d9aac7293abdf491751c8989ef9725906de926
Ignasi Fosch <natx@y10k.ws>
2015-05-27 13:00:51 +0200

    INF-865: Implements create_template in Utils module

--- lib/skyed/utils.rb
+++ lib/skyed/utils.rb
@@ -1,7 +1,22 @@
+require 'erb'
+
 module Skyed
   # This module encapsulates some generic utility functions.
   module Utils
     class << self
+      def create_template(base, subpath, template_file)
+        b = binding
+        folders = subpath.split('/')
+        template = ERB.new(
+          File.read(File.join(
+              File.dirname(File.dirname(File.dirname(__FILE__))),
+              'templates',
+              template_file)))
+        File.open(File.join(base, folders), 'w') do |f|
+          f.write(template.result b)
+        end
+      end
+
       def read_key_file(key_file)
         File.open(key_file, 'rb').read
       end
--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -1,6 +1,41 @@
 require 'spec_helper'
 require 'skyed'
 
+describe 'Skyed::Utils.create_template' do
+  let(:base)          { '/tmp' }
+  let(:subpath)       { 'file' }
+  let(:template_file) { 'some_template.erb' }
+  let(:templates_path) do
+    '/Users/ifosch/src/github.com/ifosch/skyed/templates'
+  end
+  let(:template_content) do
+    '[default]\nregion = {{ aws_default_region }}'
+  end
+  let(:template)      { double('ERB') }
+  let(:file)          { double('File') }
+  let(:b)             { double('Bindings') }
+  before(:each) do
+    expect(Skyed::Utils)
+      .to receive(:binding)
+      .and_return(b)
+    expect(File)
+      .to receive(:read)
+      .with("#{templates_path}/some_template.erb")
+      .and_return(template_content)
+    expect(ERB)
+      .to receive(:new)
+      .with(template_content)
+      .and_return(template)
+    expect(File)
+      .to receive(:open)
+      .with('/tmp/file', 'w')
+      .and_return(file)
+  end
+  it 'creates the file from template' do
+    Skyed::Utils.create_template(base, subpath, template_file)
+  end
+end
+
 describe 'Skyed::Utils.read_key_file' do
   let(:file_path)  { '/home/user/.ssh/id_rsa' }
   let(:fd)         { double('File') }
