7676df2349e456e3bd5ca6c4266ea219d54a0fdb
Ignasi Fosch <natx@y10k.ws>
2015-05-27 13:28:42 +0200

    INF-865: Makes the wrapper executable after creation

--- lib/skyed/git.rb
+++ lib/skyed/git.rb
@@ -10,6 +10,7 @@ module Skyed
          stack[:stack_id])
         ENV['PKEY'] = Skyed::Settings.opsworks_git_key
         Skyed::Utils.create_template('/tmp', 'ssh-git', 'ssh-git.erb')
+        File.chmod(0755, '/tmp/ssh-git')
         ENV['GIT_SSH'] = '/tmp/ssh-git'
         path = "/tmp/skyed.#{SecureRandom.hex}"
         ::Git.clone(stack[:custom_cookbooks_source][:url], path)
