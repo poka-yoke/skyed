f6143f60062cfb3f24118780a8321b14d80d2831
Ignasi Fosch <natx@y10k.ws>
2015-06-16 10:48:59 +0200

    INF-803: Modifies Skyed::Create to allow restore from snapshot

--- lib/skyed/create.rb
+++ lib/skyed/create.rb
@@ -4,10 +4,22 @@ module Skyed
     class << self
       def execute(_global_options, options, args)
         Skyed::Init.credentials if Skyed::Settings.empty?
-        endpoint = Skyed::AWS::RDS.create_instance(
+        endpoint = create_new(options, args) if args.length == 1
+        endpoint = restore_new(options, args) if args.length == 2
+        puts endpoint
+      end
+
+      def restore_new(options, args)
+        Skyed::AWS::RDS.create_instance_from_snapshot(
+          args[0],
+          args[1],
+          options.select { |k| k != :rds })
+      end
+
+      def create_new(options, args)
+        Skyed::AWS::RDS.create_instance(
           args[0],
           options.select { |k| k != :rds })
-        puts endpoint
       end
     end
   end
--- spec/create_spec.rb
+++ spec/create_spec.rb
@@ -3,57 +3,102 @@ require 'skyed'
 
 describe 'Skyed::Create.execute' do
   context 'when invoked with RDS option' do
-    let(:options) do
-      {
-        rds: true,
-        size: 100,
-        type: 'm1.large',
-        user: 'root',
-        pass: 'pass',
-        db_security_group_name: 'rds-launch-wizard',
-        db_parameters_group_name: 'my_db_params'
-      }
-    end
-    let(:create_args) do
-      {
-        size: 100,
-        type: 'm1.large',
-        user: 'root',
-        pass: 'pass',
-        db_security_group_name: 'rds-launch-wizard',
-        db_parameters_group_name: 'my_db_params'
-      }
-    end
-    let(:args)     { ['my-rds'] }
-    let(:endpoint) { 'my-rds.c7werpdeshqu.us-east-1.rds.amazonaws.com:5432' }
-    before(:each) do
-      expect(Skyed::AWS::RDS)
-        .to receive(:create_instance)
-        .with('my-rds', create_args)
-        .and_return(endpoint)
-    end
-    context 'and not initialized' do
+    context 'for new instance creation' do
+      let(:options) do
+        {
+          rds: true,
+          size: 100,
+          type: 'm1.large',
+          user: 'root',
+          pass: 'pass',
+          db_security_group_name: 'rds-launch-wizard',
+          db_parameters_group_name: 'my_db_params'
+        }
+      end
+      let(:create_args) do
+        {
+          size: 100,
+          type: 'm1.large',
+          user: 'root',
+          pass: 'pass',
+          db_security_group_name: 'rds-launch-wizard',
+          db_parameters_group_name: 'my_db_params'
+        }
+      end
+      let(:args)     { ['my-rds'] }
+      let(:endpoint) { 'my-rds.c7werpdeshqu.us-east-1.rds.amazonaws.com:5432' }
       before(:each) do
-        expect(Skyed::Settings)
-          .to receive(:empty?)
-          .and_return(true)
-        expect(Skyed::Init)
-          .to receive(:credentials)
+        expect(Skyed::AWS::RDS)
+          .to receive(:create_instance)
+          .with('my-rds', create_args)
+          .and_return(endpoint)
       end
-      it 'creates the RDS machine' do
-        Skyed::Create.execute(nil, options, args)
+      context 'and not initialized' do
+        before(:each) do
+          expect(Skyed::Settings)
+            .to receive(:empty?)
+            .and_return(true)
+          expect(Skyed::Init)
+            .to receive(:credentials)
+        end
+        it 'creates the RDS machine' do
+          Skyed::Create.execute(nil, options, args)
+        end
+      end
+      context 'and initialized' do
+        before(:each) do
+          expect(Skyed::Settings)
+            .to receive(:empty?)
+            .and_return(false)
+          expect(Skyed::Init)
+            .not_to receive(:credentials)
+        end
+        it 'creates the RDS machine' do
+          Skyed::Create.execute(nil, options, args)
+        end
       end
     end
-    context 'and initialized' do
-      before(:each) do
-        expect(Skyed::Settings)
-          .to receive(:empty?)
-          .and_return(false)
-        expect(Skyed::Init)
-          .not_to receive(:credentials)
+    context 'for snapshot restoring' do
+      let(:options) do
+        {
+          rds: true,
+          size: 100,
+          type: 'm1.large',
+          user: 'root',
+          pass: 'pass',
+          db_security_group_name: 'rds-launch-wizard',
+          db_parameters_group_name: 'my_db_params'
+        }
       end
-      it 'creates the RDS machine' do
-        Skyed::Create.execute(nil, options, args)
+      let(:create_args) do
+        {
+          size: 100,
+          type: 'm1.large',
+          user: 'root',
+          pass: 'pass',
+          db_security_group_name: 'rds-launch-wizard',
+          db_parameters_group_name: 'my_db_params'
+        }
+      end
+      let(:args)     { ['my-rds', 'rds:my-rds-2015-06-02-00-06'] }
+      let(:endpoint) { 'my-rds.c7werpdeshqu.us-east-1.rds.amazonaws.com:5432' }
+      before(:each) do
+        expect(Skyed::AWS::RDS)
+          .to receive(:create_instance_from_snapshot)
+          .with('my-rds', 'rds:my-rds-2015-06-02-00-06', create_args)
+          .and_return(endpoint)
+      end
+      context 'and not initialized' do
+        before(:each) do
+          expect(Skyed::Settings)
+            .to receive(:empty?)
+            .and_return(true)
+          expect(Skyed::Init)
+            .to receive(:credentials)
+        end
+        it 'creates the RDS machine' do
+          Skyed::Create.execute(nil, options, args)
+        end
       end
     end
   end
