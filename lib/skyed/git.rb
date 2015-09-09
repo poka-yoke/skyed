a062efd7258e6edb255c4a871ed5408eee28e005
Ignasi Fosch <natx@y10k.ws>
2015-09-09 12:15:44 +0200

    INF-941: Add check if remore is cloned

--- lib/skyed/git.rb
+++ lib/skyed/git.rb
@@ -14,7 +14,7 @@ module Skyed
         ENV['GIT_SSH'] = '/tmp/ssh-git'
         path = "/tmp/skyed.#{SecureRandom.hex}"
         r = ::Git.clone(stack[:custom_cookbooks_source][:url], path)
-        puts r.path
+        puts repo_path(r)
         path
       end
     end
